73bfb003d05996583a05f0de4e82a27af72c6e6a
cowilliams_deloitte <cowilliams@deloitte.com>
2024-10-10 17:00:09 -0600

    finished 5 and 7

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,7 +1,9 @@
-fn fill_vec(vec: &mut i32) -> Vec<i32> {
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(88);
+
+    vec
 }
 
 fn main() {
@@ -12,12 +14,13 @@ fn main() {
 mod tests {
     use super::*;
 
+    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
+    // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-        let mut vec1 = vec0.clone();
 
-        fill_vec(&mut vec1);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
